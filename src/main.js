7786a5f018895d693e34d6a29c46e7fd2ce11c24
Bryan Perfetto <regularkid@gmail.com>
2019-04-11 07:17:33 -0700

    Added CoolMathGames site lock.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,40 @@ var screenScale = 1.0;
 var aw = new Aw(screenWidth, screenHeight, screenScale, ["CoolmathGames-640x480.png"]);
 aw.state = splash;
 
+var avaiabledomains =
+[
+    'localhost',
+    'coolmath-games.com',
+    'edit.coolmath-games.com',
+    'stage.coolmath-games.com',
+    'edit-stage.coolmath-games.com',
+    'dev.coolmath-games.com',
+    'm.coolmath-games.com',
+    'www.coolmath-games.com', 
+    'www.coolmathgames.com', 
+    'm.coolmathgames.com', 
+    'stage.coolmathgames.com', 
+    'm-stage.coolmathgames.com', 
+    'dev.coolmathgames.com', 
+    'm-dev.coolmathgames.com', 
+    'm.cmatgame.local', 
+    'cmatgame.local'
+];
+
+var isPlayAvaiable = false;
+var domain = document.domain;
+for(var i = 0; i < avaiabledomains.length; i ++)
+{
+    if(domain.indexOf(avaiabledomains[i]) != -1)
+    {
+        isPlayAvaiable = true;
+        break;
+    }
+}
+
+// Site lock
+if(!isPlayAvaiable) { aw.state = undefined; };
+
 var level;
 var player;
 var levelIdx = 0;
