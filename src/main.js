44f1fffd49b146f7084e0da8ba0060220bf961b7
Bryan Perfetto <regularkid@gmail.com>
2019-04-10 21:22:48 -0700

    Added CoolMathGames splash screen.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ var screenWidth = 640;
 var screenHeight = 480;
 var screenScale = 1.0;
 
-var aw = new Aw(screenWidth, screenHeight, screenScale, []);
-aw.state = init;
+var aw = new Aw(screenWidth, screenHeight, screenScale, ["CoolmathGames-640x480.png"]);
+aw.state = splash;
 
 var level;
 var player;
@@ -11,6 +11,20 @@ var levelIdx = 0;
 var endLevelTime = 0;
 var lives = 5;
 var difficultyMode = 0;
+var splashTime = 1.0;
+
+function splash(deltaTime)
+{
+    splashTime -= deltaTime;
+    if (splashTime <= 0.0)
+    {
+        aw.state = init;
+        aw.mouseLeftButtonJustPressed = false;
+        aw.mouseLeftButtonJustUp = false;
+    }
+
+    aw.drawSprite({name: "CoolmathGames-640x480.png", x: 320, y: 240});
+}
 
 function init(deltaTime)
 {
